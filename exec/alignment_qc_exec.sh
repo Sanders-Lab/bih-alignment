1931fdfbea9eb8687f1606a270ff584211d9207d
benedict909 <64800230+benedict909@users.noreply.github.com>
2023-07-10 12:49:43 +0200

    Update alignment_qc_exec.sh

--- exec/alignment_qc_exec.sh
+++ exec/alignment_qc_exec.sh
@@ -188,7 +188,7 @@ printf '\n ### 7. running R script to plot QC metrics  #####\n'
 
 bigcells=$(find bam/*bam -size +1G | rev | cut -f1 -d/ | rev)
 nbigcells=$(echo $bigcells | wc -w)
-[ $nbigcells -ge 1 ] bigcells > /fast/groups/ag_sanders/scratch/sequencing_tmp/${project_name}/bigcells.txt
+[ $nbigcells -ge 1 ] && bigcells > /fast/groups/ag_sanders/scratch/sequencing_tmp/${project_name}/bigcells.txt
 
 Rscript ${SLURM_SUBMIT_DIR}/bih-alignment/exec/alignment_qc_exec.R \
 	$project_name \
