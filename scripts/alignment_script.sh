bf2f860644b5a4100cc2a2e6afa85d0285a4e2d2
benedict909 <64800230+benedict909@users.noreply.github.com>
2024-04-30 14:38:53 +0200

    Update alignment_script.sh
    rm .fastq.gz suffix hard coding

--- scripts/alignment_script.sh
+++ scripts/alignment_script.sh
@@ -33,6 +33,11 @@ n_threads=$(nproc) # number of threads given to slurm job, for highest efficienc
 n_threads_divided=$(expr $n_threads / 4)
 submit_dir=$SLURM_SUBMIT_DIR
 
+# Find the index of the first occurrence of '1'
+index=$(expr index "$mate1_suffix" "1")
+# Keep the substring after the first occurrence of '1'
+allmates_suffix="${mate1_suffix:$index}"
+
 # set reference genome
 if [ $organism = 'human' ]
 then
@@ -151,7 +156,7 @@ multiqc_dir=${qc_dir}/multiqc ; mkdir -m 775 $multiqc_dir
 
 # run FastQC to generate quality metrics on
 echo "Running FastQC"
-fastqc -q -t $n_threads -o ${fastqc_dir} ${fastq_dir}/*fastq.gz
+fastqc -q -t $n_threads -o ${fastqc_dir} ${fastq_dir}/*${allmates_suffix}
 
 # run multiqc
 echo "Running MutiQC to combine FastQC results"
@@ -165,7 +170,7 @@ echo "MutiQC results saved to: ${multiqc_dir}"
 printf '\n ### 5. Running alignment #####\n'
 
 # make unique list of library names by removing suffixes
-libraries=$(ls $fastq_dir/*fastq.gz \
+libraries=$(ls $fastq_dir/*${allmates_suffix}\
 	| sed -e "s?${fastq_dir}/??g;s?${mate1_suffix}??g;s?${mate2_suffix}??g" \
 	| sort -u)
 
