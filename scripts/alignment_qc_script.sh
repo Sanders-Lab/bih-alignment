7d881e0e02ced5ce0670f3bc28565c0347fde014
MartinaMac <160611605+MartinaMac@users.noreply.github.com>
2025-01-21 15:53:19 +0100

    Update alignment_qc_script.sh

--- scripts/alignment_qc_script.sh
+++ scripts/alignment_qc_script.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00
-#SBATCH --mem=128G
+#SBATCH --mem-per-cpu=2G
 
 # script for running alignment QC *separately* to alignment pipeline (alignment must have already been run successfully)
 # April 2024
